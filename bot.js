8a84a0bc9eff06b0a3117a4d492806a5202bda63
ahmedmaher10 <43995604+ahmedmaher10@users.noreply.github.com>
2018-10-11 18:24:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -416,4 +416,15 @@ client.on('message', message => {
   })
 }
 });
+client.on('message', message => {
+var prefix = "-";
+      if(message.content === "!hide") {
+      if(!message.channel.guild) return;
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You Dont Have Perms ❌');
+             message.channel.overwritePermissions(message.guild.id, {
+             READ_MESSAGES: false
+ })
+              message.channel.send('Channel Hided Successfully ! ✅  ')
+ }
+});
 client.login(process.env.BOT_TOKEN);
